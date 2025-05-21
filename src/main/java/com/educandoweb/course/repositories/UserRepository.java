51454a3783019f3b905d0fad889caea7a33c2e1f
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-05-21 10:03:12 -0300

    Adiciona camada de serviço e registro de componentes

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.repositories;
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Pordeira colocar, mas a minha interface está herdando do JPArepository que já está registrado como oponente do string
+//@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 }
