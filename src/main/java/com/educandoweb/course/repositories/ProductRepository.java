e6598d3265a46d9f802f80b344710f5f5e8295f2
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-05-23 19:15:46 -0300

    Criando a entidade Product

---
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Pordeira colocar, mas a minha interface está herdando do JPArepository que já está registrado como oponente do string
+//@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
