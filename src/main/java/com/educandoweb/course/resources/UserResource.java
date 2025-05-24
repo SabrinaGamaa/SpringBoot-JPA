39c9facc31276eb27f66716bcfea7d6cfd6f6a6a
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-05-24 15:04:21 -0300

    Inserção de User com status 201 created

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,17 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public  ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj); // Salva o objeto no banco e retorna com ID preenchido
+
+        URI uri = ServletUriComponentsBuilder   // Monta a URI do recurso criado: ex. http://localhost:8080/users/5
+                .fromCurrentRequest()          // Pega a URL atual da requisição (ex. /users)
+                .path("/{id}")                // Adiciona /{id} no final da URL
+                .buildAndExpand(obj.getId()) // Substitui {id} pelo valor real do ID
+                .toUri();                   // Converte para objeto URI
+        return ResponseEntity.created(uri).body(obj); // Retorna 201 Created com Location no cabeçalho
     }
 }
